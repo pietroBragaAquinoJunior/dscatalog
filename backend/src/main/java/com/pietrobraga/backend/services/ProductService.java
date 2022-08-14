ed1f8e30757a9f119bf150af1d5435b819a23501
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-14 15:30:47 -0300

    novo Pageable

--- backend/src/main/java/com/pietrobraga/backend/services/ProductService.java
+++ backend/src/main/java/com/pietrobraga/backend/services/ProductService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,8 +29,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Product> products = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+		Page<Product> products = repository.findAll(pageable);
 		return products.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 	
